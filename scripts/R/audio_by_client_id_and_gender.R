f3d5cb3b937b1e10abbb0f00403082d34f98806c
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2022-06-07 01:19:51 -0500

    👩‍💻Complete script to sort and copy by gender and speaker

--- scripts/R/audio_by_client_id_and_gender.R
+++ scripts/R/audio_by_client_id_and_gender.R
@@ -1,5 +1,6 @@
 # This script extracts the audio clips from `validated.tsv` and sorts them
-# into separate folder by gender value (i.e. NA, "other", "female", "male")
+# into separate folder by gender value = "female", "male"
+# as well as by different speakers, i.e. by client_id
 
 ## Import R packages ##########################################################
 
@@ -33,9 +34,9 @@ female_client_ids <- tb %>%
 num_female_speakers <- nrow(female_client_ids)
 
 ### Create output directories for each female speakers
-female_speaker_base_path <- 
+female_speaker_base_path <-
   "./data/unprocessed/female_audio/female_speaker_"
-female_speaker_paths <- 
+female_speaker_paths <-
   paste0(female_speaker_base_path, 1:num_female_speakers)
 
 for (i in 1:num_female_speakers){
@@ -55,13 +56,35 @@ for (i in 1:num_female_speakers){
   file.copy(speaker_clips, female_speaker_paths[i])
 }
 
+## Male speakers ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+### Character vector of male speakers' client id's
+male_client_ids <- tb %>%
+  select(client_id, gender) %>%
+  filter(gender == "male") %>%
+  distinct(client_id)
 
+num_male_speakers <- nrow(male_client_ids)
 
+### Create output directories for each male speakers
+male_speaker_base_path <- 
+  "./data/unprocessed/male_audio/male_speaker_"
+male_speaker_paths <- 
+  paste0(male_speaker_base_path, 1:num_male_speakers)
 
+for (i in 1:num_male_speakers){
+  dir.create(male_speaker_paths[i], recursive = TRUE)
+}
 
-
-
-
-
-
+### Get the audio clip paths for each male speaker
+### and copy them to the appropriate output directory
+for (i in 1:num_male_speakers){
+  speaker_clips <- tb %>%
+    select(client_id, gender, path) %>%
+    filter(client_id %in% male_client_ids[i,], gender == "male") %>%
+    select(path) %>%
+    as_vector() %>%
+    paste(cv_corpus_clips_path, ., sep="/")
+  
+  file.copy(speaker_clips, male_speaker_paths[i])
+}
