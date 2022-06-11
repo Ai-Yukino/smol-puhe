0509a2d1a5d624a840d84cecd9a6ea31897cd9c6
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2022-06-11 13:50:52 -0500

    👩‍💻Add more descriptive variable names

--- AiTriesStuff.R
+++ AiTriesStuff.R
@@ -28,17 +28,17 @@ ggplot(tb, aes(x=f0, y=gender)) +
        subtitle = "gender ~ f0",
        caption = "Data: smol-puhe/data/filtered_data.csv")
 
-ggsave("logistic_gender_f0.png")
+# ggsave("logistic_gender_f0.png")
 
-model1 <- glm(gender~f0, data = tb)
-summary(model1)
+model_f0 <- glm(gender~f0, data = tb)
+summary(model_f0)
 
 ## Logistic regression ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## gender ~ f0 / f1
 
-tb$quotient1 <- tb$f0 / tb$f1
+tb$f0_over_f1 <- tb$f0 / tb$f1
 
-ggplot(tb, aes(x=quotient1, y=gender)) +
+ggplot(tb, aes(x=f0_over_f1, y=gender)) +
   geom_point(alpha=.5, aes(color = cut(tb$gender, c(-1, 0.5, 1)))) +
   scale_color_manual(name = "Gender",
                      values = c("(-1,0.5]" = "#ff0080",
@@ -51,17 +51,17 @@ ggplot(tb, aes(x=quotient1, y=gender)) +
        caption = "Data: smol-puhe/data/filtered_data.csv") + 
   xlab("f0 / f1")
 
-ggsave("logistic_gender_f0_over_f1.png")
+# ggsave("logistic_gender_f0_over_f1.png")
 
-model1 <- glm(gender~quotient1, data = tb)
-summary(model1)
+model_f0_over_f1 <- glm(gender~f0_over_f1, data = tb)
+summary(model_f0_over_f1)
 
 ## Logistic regression ########################################################
 ## gender ~ f1 / f0
 
-tb$quotient2 = tb$f1 / tb$f0
+tb$f1_over_f0 = tb$f1 / tb$f0
 
-ggplot(tb, aes(x=quotient2, y=gender)) +
+ggplot(tb, aes(x=f1_over_f0, y=gender)) +
   geom_point(alpha=.5, aes(color = cut(tb$gender, c(-1, 0.5, 1)))) +
   scale_color_manual(name = "Gender",
                      values = c("(-1,0.5]" = "#ff0080",
@@ -75,17 +75,17 @@ ggplot(tb, aes(x=quotient2, y=gender)) +
        caption = "Data: smol-puhe/data/filtered_data.csv") + 
   xlab("f1 / f0")
 
-ggsave("logistic_gender_f1_over_f0.png")
+# ggsave("logistic_gender_f1_over_f0.png")
 
-model1 <- glm(gender~quotient2, data = tb)
-summary(model1)
+model_f1_over_f0 <- glm(gender~f1_over_f0, data = tb)
+summary(model_f1_over_f0)
 
 ## Logistic regression ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## gender ~ f1 - f0
 
-tb$difference = tb$f1 - tb$f0
+tb$f1_minus_f0 = tb$f1 - tb$f0
 
-ggplot(tb, aes(x=difference, y=gender)) +
+ggplot(tb, aes(x=f1_minus_f0, y=gender)) +
   geom_point(alpha=.5, aes(color = cut(tb$gender, c(-1, 0.5, 1)))) +
   scale_color_manual(name = "Gender",
                      values = c("(-1,0.5]" = "#ff0080",
@@ -99,10 +99,10 @@ ggplot(tb, aes(x=difference, y=gender)) +
        caption = "Data: smol-puhe/data/filtered_data.csv") + 
   xlab("f1 - f0")
 
-ggsave("logistic_gender_f1_minus_f0.png")
+# ggsave("logistic_gender_f1_minus_f0.png")
 
-model1 <- glm(gender~difference, data = tb)
-summary(model1)
+model_f1_minus_f0 <- glm(gender~f1_minus_f0, data = tb)
+summary(model_f1_minus_f0)
 
   # References __________________________________________________________________
 # https://www.statology.org/plot-logistic-regression-in-r/
